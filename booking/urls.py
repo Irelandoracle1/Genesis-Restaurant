21b747c7bcbf3f60f18588d76ef1285203026831
Irelandoracle1 <nairajnr1@gmail.com>
2023-04-21 14:48:53 +0000

    added a make make booking view with template

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('make-booking', views.BookingCreation.as_view(), name='booking_create'),
     ]
--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from .models import Booking
 from django.views import generic
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+# This is the HomePage View
 
 
 class HomePage(generic.ListView):
     model = Booking
     template_name = 'booking/index.html'
+
+
+# This is the Booking Creation Page
+
+
+class BookingCreation(generic.CreateView):
+    model = Booking
+    template_name = 'booking/booking_page.html'
+    fields = ['first_name', 'last_name', 'email', 'phone', 'date', 'time', 'guests_number', 'special_message']
+    success_url = reverse_lazy('home')
