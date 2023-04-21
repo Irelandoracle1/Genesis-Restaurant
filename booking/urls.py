ec041093590bf6b4f1d645f171eb2ea78aac0fd4
Irelandoracle1 <nairajnr1@gmail.com>
2023-04-21 22:37:01 +0000

    added a Booking List View and the corresponding template

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('make-booking', views.BookingCreation.as_view(), name='booking_create'),
+    path('make-booking',
+         views.BookingCreation.as_view(),
+         name='booking_create'),
+    path('booking-list/',
+         views.BookingList.as_view(),
+         name='booking_detail'),
     ]
--- booking/views.py
+++ booking/views.py
@@ -21,10 +21,16 @@ class BookingCreation(LoginRequiredMixin, generic.CreateView):
     model = Booking
     template_name = 'booking/booking_page.html'
     fields = ['first_name', 'last_name', 'email', 'phone', 'date', 'time', 'guests_number', 'special_message']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('booking_detail')
 
     def form_valid(self, form):
         booking = form.save(commit=False)
         booking.user = self.request.user
         booking.save()
         return super().form_valid(form)
+
+
+class BookingList(LoginRequiredMixin, generic.ListView):
+    model = Booking
+    template_name = 'booking/booking_detail.html'
+    context_object_name = 'bookings'
