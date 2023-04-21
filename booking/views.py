9c25cf5e6b22be41c0ea6e2fc78fbca00abf97b7
Irelandoracle1 <nairajnr1@gmail.com>
2023-04-21 21:12:42 +0000

    added form validation and savingfunction to booking creation view and added loginrequiredmixin torequire login before booking

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Booking
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -16,8 +17,14 @@ class HomePage(generic.ListView):
 # This is the Booking Creation Page
 
 
-class BookingCreation(generic.CreateView):
+class BookingCreation(LoginRequiredMixin, generic.CreateView):
     model = Booking
     template_name = 'booking/booking_page.html'
     fields = ['first_name', 'last_name', 'email', 'phone', 'date', 'time', 'guests_number', 'special_message']
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        booking = form.save(commit=False)
+        booking.user = self.request.user
+        booking.save()
+        return super().form_valid(form)
