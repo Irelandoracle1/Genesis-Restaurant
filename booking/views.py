9844eb54bad0df46d60fbdf6557e36d957ba6f95
Irelandoracle1 <nairajnr1@gmail.com>
2023-04-21 13:31:58 +0000

    çreated a view class for the hom page created a template directory and file for it and added contents to it along with base.html file which is being extended

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Booking
+from django.views import generic
 
 # Create your views here.
+
+
+class HomePage(generic.ListView):
+    model = Booking
+    template_name = 'booking/index.html'
